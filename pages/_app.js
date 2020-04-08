99a6403ec0935e48c88ad9e3603e5621b2272b2d
Dean Wake <deanmwake@gmail.com>
2020-04-09 00:30:54 +0200

    feat(init): Initialize NextJs, Redux, Components

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import NextApp from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import makeStore from '../store';
+import { elementType, object } from 'prop-types';
+
+import { Provider } from 'react-redux';
+import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+
+import theme from '../theme';
+
+class App extends NextApp {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, store } = this.props;
+
+    return (
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <ColorModeProvider>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </ColorModeProvider>
+      </ThemeProvider>
+    );
+  }
+};
+
+App.propTypes = {
+  Component: elementType,
+  store: object,
+}
+
+export default withRedux(makeStore)(App);
