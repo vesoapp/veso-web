9dd9c32a8d90cdb1f9fa149bcd7f9e2c09f4d9b8
Danny Michel <dan@varmag.com>
2020-04-10 13:15:29 -0400

    add placeholder video to trailer slider

--- components/TrailerSlider.js
+++ components/TrailerSlider.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Flex } from "@chakra-ui/core";
+import { Flex, Box, AspectRatioBox } from "@chakra-ui/core";
 
-const TrailerSlider = ({children}) => (
-    <Flex>
-        {children}
-    </Flex>
+const TrailerSlider = () => (
+    <div className="video-slider-container">
+    <AspectRatioBox ratio={16 / 9}>
+    <Box
+      as="iframe"
+      title="slider-video"
+      maxH="50vh"
+      src="https://www.youtube.com/embed/U3Xy2x9NDrw"
+      allowFullScreen
+    />
+  </AspectRatioBox>
+  </div>
 );
 
 export default TrailerSlider;
