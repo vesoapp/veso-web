ea5b8b5ac0747d94c30b04278775f53cb30b3c2c
Dean Wake <deanmwake@gmail.com>
2020-04-20 01:14:02 +0200

    feat(docz): Initial docz setup example header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Header.module.css';
+
+const Header: React.FunctionComponent = () => (
+  <div className={style.Header}>
+    <h1>Example Header</h1>
+  </div>
+);
+
+export default Header;
