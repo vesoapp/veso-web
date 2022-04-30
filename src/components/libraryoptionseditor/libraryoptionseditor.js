1e01fc609a22d6756b29c5b08d515d678fa270a2
sancodes <munsan14@gmail.com>
2022-04-30 13:04:58 -0700

    fix no-sequence lint

--- src/components/libraryoptionseditor/libraryoptionseditor.js
+++ src/components/libraryoptionseditor/libraryoptionseditor.js
@@ -64,7 +64,7 @@ import template from './libraryoptionseditor.template.html';
         let html = '';
         const elem = page.querySelector('.metadataReaders');
 
-        if (plugins.length < 1) return elem.innerHTML = '', elem.classList.add('hide'), !1;
+        if (plugins.length < 1) return (elem.innerHTML = '', elem.classList.add('hide'), !1);
         html += `<h3 class="checkboxListLabel">${globalize.translate('LabelMetadataReaders')}</h3>`;
         html += '<div class="checkboxList paperList checkboxList-paperList">';
         for (let i = 0; i < plugins.length; i++) {
@@ -97,7 +97,7 @@ import template from './libraryoptionseditor.template.html';
     function renderMetadataSavers(page, metadataSavers) {
         let html = '';
         const elem = page.querySelector('.metadataSavers');
-        if (!metadataSavers.length) return elem.innerHTML = '', elem.classList.add('hide'), false;
+        if (!metadataSavers.length) return (elem.innerHTML = '', elem.classList.add('hide'), false);
         html += `<h3 class="checkboxListLabel">${globalize.translate('LabelMetadataSavers')}</h3>`;
         html += '<div class="checkboxList paperList checkboxList-paperList">';
         for (let i = 0; i < metadataSavers.length; i++) {
