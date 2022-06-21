6cc62c9f1d10140f46e64b54ca212935bf98cdbf
Danny Michel <dannymichel@users.noreply.github.com>
2022-06-21 08:47:50 -0400

    renaming, colors, and readme.md

--- .eslintrc.js
+++ .eslintrc.js
@@ -200,7 +200,7 @@ module.exports = {
                 // Dependency globals
                 '$': 'readonly',
                 'jQuery': 'readonly',
-                // Jellyfin globals
+                // Veso globals
                 'ApiClient': 'writable',
                 'Events': 'writable',
                 'chrome': 'writable',
