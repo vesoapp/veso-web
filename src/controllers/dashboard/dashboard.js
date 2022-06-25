560235f285bdeae4dc3b3bb3c9db118b538193cc
Danny Michel <dannymichel@users.noreply.github.com>
2022-06-25 03:28:04 -0400

    Switch to veso red colors.

--- src/controllers/dashboard/dashboard.js
+++ src/controllers/dashboard/dashboard.js
@@ -373,7 +373,7 @@ import confirm from '../../components/confirm/confirm';
                 html += '<progress max="100" value="' + progress + '" title="' + progress + '%">';
                 html += progress + '%';
                 html += '</progress>';
-                html += "<span style='color:#00a4dc;margin-left:5px;margin-right:5px;'>" + progress + '%</span>';
+                html += "<span style='color:#d61e30;margin-left:5px;margin-right:5px;'>" + progress + '%</span>';
                 html += '<button type="button" is="paper-icon-button-light" title="' + globalize.translate('ButtonStop') + '" onclick="DashboardPage.stopTask(this, \'' + task.Id + '\');" class="autoSize"><span class="material-icons cancel" aria-hidden="true"></span></button>';
             } else if (task.State === 'Cancelling') {
                 html += '<span style="color:#cc0000;">' + globalize.translate('LabelStopping') + '</span>';
