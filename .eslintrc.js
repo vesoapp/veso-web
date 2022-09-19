9057524269285720ff8a089b0a4d2e411cbb7098
Danny Michel <dan@varmag.com>
2022-09-19 04:54:42 -0400

    Rename

--- .eslintrc.js
+++ .eslintrc.js
@@ -202,7 +202,7 @@ module.exports = {
                 // Dependency globals
                 '$': 'readonly',
                 'jQuery': 'readonly',
-                // Jellyfin globals
+                // Veso globals
                 'ApiClient': 'writable',
                 'Events': 'writable',
                 'chrome': 'writable',
